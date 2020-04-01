1dc41fadb2b3931f72cf8a8826c5b3394f32d779
jselmani <jselmanovski1@myseneca.ca>
2020-03-31 20:30:25 -0400

    Completed Quizzler app with added challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import './quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,34 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userAnswer) {
+    bool cAnswer = quizBrain.getAnswer();
+
+    if (quizBrain.isFinished()) {
+      Alert(
+              context: context,
+              title: "All done!",
+              desc: "Close this alert and start again!")
+          .show();
+      setState(() {
+        quizBrain.reset();
+        scoreKeeper.clear();
+      });
+    } else {
+      setState(() {
+        if (userAnswer == cAnswer) {
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+        } else {
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+        }
+
+        quizBrain.nextQuestion();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +69,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +93,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                //user pressed true
+                checkAnswer(true);
               },
             ),
           ),
@@ -79,12 +112,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                //user pressed false
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String question;
+  bool answer;
+
+  // short hand that tells Dart to take the args passed to initialize the
+  // properties of the class
+  Question(this.question, this.answer);
+}
